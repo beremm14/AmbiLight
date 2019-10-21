5658d51adb3c49f7f2f6faaaa71548c34748c755
Emil Berger <berger@student.tugraz.at>
2019-10-21 10:05:59 +0200

    Add: Init components

--- AmbiLight/AmbiLight/main.c
+++ AmbiLight/AmbiLight/main.c
@@ -3,14 +3,51 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+uint8_t led_active = PB0;
+uint8_t led1 = PB1;
+uint8_t led2 = PB2;
+uint8_t led3 = PB3;
+uint8_t led4 = PB4;
+
+uint8_t taster = PD2;
+uint8_t out = PD3;
+
+enum States {
+    OFF,
+    ON,
+    T1,
+    T2,
+    T3,
+    T4
+};
+
+enum States state;
+
 void initIO(void) {
-	
+    DDRB |= (1<<led_active | 1<<led1 | 1<<led2 | 1<<led3 | 1<<led4);
+    DDRD |= (1<<out);
+    PORTD |= (1<<taster);
+    state = OFF;
 }
 
 int main(void) {
 	initIO();
 
 	while (1) {
+        switch (state) {
+            case OFF:
+                
+            case ON:
+                
+            case T1:
+                
+            case T2:
+                
+            case T3:
+                
+            case T4:
+                
+        }
 	}
 	return 0; // never reached
 }
